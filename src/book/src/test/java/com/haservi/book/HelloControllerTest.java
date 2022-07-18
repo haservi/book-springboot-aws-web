e2b623eb980ae47c12c1f0d3783bbb1605de7403
Haseop Lee <haservi@naver.com>
2022-07-19 00:59:38 +0900

    Fixed 테스트 코드 오류 수정 Mock관련

--- src/book/src/test/java/com/haservi/book/HelloControllerTest.java
+++ src/book/src/test/java/com/haservi/book/HelloControllerTest.java
@@ -1,10 +1,9 @@
 package com.haservi.book;
 
-import com.haservi.book.web.HelloController;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,21 +14,22 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.hamcrest.Matchers.is;
 
 
-//@RunWith(SpringRunner.class) // JUnit Vintage?
-@WebMvcTest(controllers = HelloController.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class) // JUnit Vintage?
+//@WebMvcTest(controllers = HelloController.class)
+@SpringBootTest
+@AutoConfigureMockMvc // MockMvc 자동 설정
 public class HelloControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @Test
     public void hello_return() throws Exception {
         String hello = "hello";
 
-        mvc.perform(MockMvcRequestBuilders.get("/hello"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.content().string(hello)); // hello 인지 검증
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello)); // hello 인지 검증
     }
 
     @Test
@@ -38,13 +38,12 @@ public class HelloControllerTest {
         String name = "hello";
         int amount = 1000;
 
-        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
-            .param("name", name)
-            .param("amount", String.valueOf(amount))
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.name", is(name))) // json 응답값 확인용
-            .andExpect(MockMvcResultMatchers.jsonPath("$.amount", is(amount)));
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", is(name))) // json 응답값 확인용
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", is(amount)));
 
     }
 
